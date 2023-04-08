3330d9f00590e63c106afa75924095f482f1a043
matteo107 <matteo107@gmail.com>
2023-04-08 11:56:12 +0300

    Updated routes to forbid only mutating endpoints for unauthorized users

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -2,17 +2,17 @@ package main
 
 import "net/http"
 import "github.com/julienschmidt/httprouter"
+import "github.com/justinas/alice"
 
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
+	standardMiddleware := alice.New()
+
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/company/:id", app.GetCompanyHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/company", app.CreateCompanyHandler)
-	router.HandlerFunc(http.MethodPatch, "/v1/company/:id", app.UpdateCompanyHandler)
-	router.HandlerFunc(http.MethodDelete, "/v1/company/:id", app.DeleteCompanyHandler)
+	router.Handler(http.MethodPost, "/v1/company", standardMiddleware.Append(app.authenticate).ThenFunc(app.CreateCompanyHandler))
+	router.Handler(http.MethodPatch, "/v1/company/:id", standardMiddleware.Append(app.authenticate).ThenFunc(app.UpdateCompanyHandler))
+	router.Handler(http.MethodDelete, "/v1/company/:id", standardMiddleware.Append(app.authenticate).ThenFunc(app.DeleteCompanyHandler))
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
-	if app.config.env == "development" {
-		return router
-	}
-	return app.authenticate(router)
+	return standardMiddleware.Then(router)
 }
